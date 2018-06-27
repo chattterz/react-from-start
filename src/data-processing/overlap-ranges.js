a368629830c955466073fd5d07bf9ba324e54c2b
Radek Anuszewski <radekqwerty@gmail.com>
2018-06-27 14:56:03 +0200

    Zakresy.

--- src/data-processing/overlap-ranges.js
+++ src/data-processing/overlap-ranges.js
@@ -1,5 +1,23 @@
 import 'ramda'
 
+const employeTwo = {
+    name: 'Justin',
+    workHistory: [
+        {
+            from: '01-2015',
+            to: '01-2016'
+        },
+        {
+            from: '06-2015',
+            to: '06-2017'
+        },
+        {
+            from: '01-2017',
+            to: '01-2018'
+        },
+    ]
+}
+
 const employeOne = {
   name: 'Diana',
   workHistory: [
@@ -24,24 +42,43 @@ const employeOne = {
       to: '07-2017'
     }
   ]
-}
+};
 
-const employeTwo = {
-  name: 'Justin',
-  workHistory: [
-    {
-      from: '01-2015',
-      to: '01-2016'
-    },
-    {
-      from: '06-2015',
-      to: '06-2017'
-    },
-    {
-      from: '01-2017',
-      to: '01-2018'
-    },
-  ]
-}
+const stringToDate = (date) => {
+  const [month, year] = date.split('-');
+  return new Date(Number(year), Number(month) - 1);
+};
+
+const interfere = (first, second) => {
+  if ((!first) || (!first.to)) {
+    return false;
+  }
+  return first.to.getTime()
+      >=
+      second.from.getTime();
+};
+
+employeTwo
+    .workHistory
+    .map(({from, to}) => {
+      return {
+        from: stringToDate(from),
+        to: stringToDate(to),
+      }
+    })
+    .sort((a, b) => {
+      return a.from.getTime() > b.from.getTime();
+    })
+    .reduce((acc, currentValue) => {
+      if (interfere(acc[acc.length - 1], currentValue)) {
+         acc[acc.length-1].to = currentValue.to;
+      }
+      else {
+          acc.push(currentValue);
+      }
+      return acc;
+    }, []);
+
+
 
 const employees = [ employeOne, employeTwo ]
