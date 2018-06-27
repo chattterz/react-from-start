827fd4eba9fc26c05324ce6b93c52901bb8e2e2e
Radek Anuszewski <radekqwerty@gmail.com>
2018-06-27 17:37:49 +0200

    Podstawowe wiadomości

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import sortedElements from './data-processing/current-date';
+import List from "@material-ui/core/es/List/List";
+import Card from "@material-ui/core/es/Card/Card";
+import CardContent from "@material-ui/core/es/CardContent/CardContent";
+import Typography from "@material-ui/core/es/Typography/Typography";
 
 export class TestComponent extends Component {
   render() {
@@ -14,17 +19,44 @@ export class TestComponent extends Component {
   }
 }
 
+const info = 'View';
+
+const DateView = ({date, isTheSameYear}) => {
+    const display =  isTheSameYear? date.toLocaleString(): 'More than year ago';
+    return (
+        <Typography color="textSecondary">
+            {display}
+        </Typography>
+    );
+};
+
+const MessageView = ({msg, author, isTheSameYear, date}) => {
+    return (
+        <div style={{maxWidth: 300}}>
+            <Card>
+                <CardContent>
+                    <Typography gutterBottom variant={"headline"} color="textSecondary" component="h2">
+                        Author: {author}
+                    </Typography>
+                    <Typography color="textSecondary">
+                        Message: {msg}
+                    </Typography>
+                    <DateView date={date} isTheSameYear={isTheSameYear} />
+                </CardContent>
+            </Card>
+        </div>
+    );
+};
+
+const MessagesListView = (messages) => {
+    return messages.map(MessageView)
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {MessagesListView(sortedElements)}
       </div>
     )
   }
--- src/data-processing/current-date.js
+++ src/data-processing/current-date.js
@@ -2,18 +2,18 @@ import 'ramda'
 
 const messages = [
   { author: 'Diana', msg: 'Hello guys, how you doooin?',
-    date: new Date(2018, 6, 27, 10, 24), },
+    date: new Date(2018, 5, 27, 10, 24), },
 
   { author: 'Jake', msg: `I'm doing great!`,
-    date: new Date(2018, 6, 27, 10, 25, 21), },
+    date: new Date(2018, 5, 27, 10, 25, 21), },
 
   { author: 'Maria', msg: `Well, I'd like a cookie :(`,
-    date: new Date(2018, 6, 27, 10, 29, 30), },
+    date: new Date(2016, 5, 27, 10, 29, 30), },
 ]
 
 const currentDate = new Date();
 
-messages.map((x) => {
+const sortedElements = messages.map((x) => {
     return {...x,
         year: x.date.getFullYear(),
         month: x.date.getMonth(),
@@ -27,3 +27,5 @@ messages.map((x) => {
       isTheSameMonthDay: x.monthDay === currentDate.getDate(),
   }
 });
+
+export default sortedElements;
