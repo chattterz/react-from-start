93eeb50769889483055de39702e6eb666611538a
5swistacz <5swistacz@fis.agh.edu.pl>
2018-06-27 12:40:24 +0200

    rozszerzenie obiektu

--- src/data-processing/current-date.js
+++ src/data-processing/current-date.js
@@ -10,3 +10,18 @@ const messages = [
   { author: 'Maria', msg: `Well, I'd like a cookie :(`,
     date: new Date(2018, 6, 27, 10, 29, 30), },
 ]
+const currentDate = new Date();
+const messagesWithDataInfo = messages.map(
+  x => ({...x, year: x.date.getFullYear(),
+     month: x.date.getMonth(),
+     monthDay: x.date.getDate(),
+
+
+})).map(
+  x => ({
+    ...x,
+    isTheSameYear: currentDate.getFullYear() === x.year,
+    isTheSameMonth: currentDate.getMonth() === x.month,
+    isTheSameMonthDay: currentDate.getDate() === x.monthDay,
+  })
+)
