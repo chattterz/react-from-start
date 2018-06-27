365051f69f6e69b5793f0b6482ff31a3f96d13d4
mipp <mipp@ailleron.com>
2018-06-27 12:34:55 +0200

    Added sameYear

--- src/data-processing/current-date.js
+++ src/data-processing/current-date.js
@@ -10,3 +10,17 @@ const messages = [
   { author: 'Maria', msg: `Well, I'd like a cookie :(`,
     date: new Date(2018, 6, 27, 10, 29, 30), },
 ]
+const addFieldSinceNow = x=> ({...x,sinceNow:null})
+
+var mappedMessages = messages.map(addFieldSinceNow)
+
+  const currentDate = new Date();
+  const messagesWithDateInfo = messages.map(
+    x => (
+      { ...x,
+         year: x.date.getFullYear(),
+         month: x.date.getMonth(), 
+         monthDay: x.date.getDate() }
+    ))
+messagesWithDateInfo.map(
+  x => ({ ...x, sameYear: x.year == currentDate.getFullYear() }))
